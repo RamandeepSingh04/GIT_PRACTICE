0bfe04ac4f5f55c93a52b14a7ceefa2831acc48d
Ramandeep Singh <raman.soin2004@gmail.com>
2024-12-16 23:16:13 +0530

    built sidebar

--- application.py
+++ application.py
@@ -7,3 +7,11 @@ st.text('helloooo')
 b = st.button(label='Login')
 if b:
     st.text('WELCOME TO THIS TUTORIAL')
+
+
+
+st.sidebar.header("Filters")
+age = st.sidebar.slider("Age", 18, 65, 25)
+gender = st.sidebar.radio("Gender", ["Male", "Female", "Other"])
+
+st.write(f"You selected: Age {age}, Gender {gender}")
